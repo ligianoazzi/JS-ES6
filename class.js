29222baef7da85687eaf4625d41386d3002e7454
Ligiano Azzi <ldaazzi@gmail.com>
2020-02-18 23:26:19 +0000

    9 - es6 modules

---
+++ class.js
@@ -0,0 +1,14 @@
+export class People {
+	constructor(name, height) {
+		this.name = name;
+		this.height = height;		
+	}
+
+	getName(){
+		return this.name;
+	}
+
+	getHeight(){
+		return this.height;
+	}
+}
